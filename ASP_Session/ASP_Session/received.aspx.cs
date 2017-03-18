cb159a1d3fa51e2b4a2dbd566aea52797a538347
tomy11 <ohnemotive74@gmail.com>
2017-03-18 20:01:37 +0700

    Add files via upload

---
+++ ASP_Session/ASP_Session/received.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASP_Session
+{
+    public partial class received : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if ((Session["firstname"] != null) && (Session["lastname"] != null))
+            {
+                lbfirstname.Text = Session["firstname"].ToString();
+                lblastname.Text = Session["lastname"].ToString();
+                Session.Remove("firstname");
+                Session.Remove("lastname");
+            }
+        }
+    }
+}
